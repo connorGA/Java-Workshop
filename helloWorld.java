93cc28eace05083d3e5af51312dfc2fb3ef23e18
connorGA <ronnoccap@gmail.com>
2023-04-04 18:03:26 -0700

    helloWorld in Java

---
+++ helloWorld.java
@@ -0,0 +1,7 @@
+// package packagename;     the package statement would go at the top, not sure what it is for this
+
+public class helloWorld {
+    public static void main(String args[]) {
+        System.out.println("Hello World");     // this is how you do a print statement
+    }
+}
