27b04bd9eba7b34b7fbc2832eeb5842629b63ed6
connorGA <ronnoccap@gmail.com>
2023-04-04 17:33:54 -0700

    classes, methods, and access modifiers

---
+++ basics.java
@@ -0,0 +1,16 @@
+ class Main {         // defines class 'Main'
+    void main() {    // method = function that belongs to a class
+        //main
+    }
+}
+
+// In Java, classes and methods should have an "access modifier". 
+// An access modifier is a special keyword that determines if other classes and methods can access those classes/methods
+// Example: public, private, etc.
+
+public class Basics {   // NOTE: the public type must be defined in its own file. So this works because the file and class are both called 'Basics'
+    public void main() {
+        //main
+    }
+}
+
